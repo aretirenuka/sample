b5d627091e8d204662892f6d462ab7038f43b578
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2022-04-14 13:04:24 +0530

    Add files via upload

---
+++ Commandline.java
@@ -0,0 +1,9 @@
+Class Commandline
+{
+public static void main(string args[])
+{
+System.out.println("the command line arguments are:");
+for(int i=0;i<args.length;i++)
+System.out.println(args[i]);
+}
+}
